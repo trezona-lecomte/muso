f94d7933d9f0ff0c26e78a5fd6c1c1485d1aa79e
trezona-lecomte <trezona.lecomte@gmail.com>
2015-03-16 22:19:25 +1300

    Add basic album validity test

--- test/models/album_test.rb
+++ test/models/album_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class AlbumTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  setup do
+    @album = Album.new(name: "Cucumber Castle")
+  end
+
+  test 'should be valid' do
+    assert @album.valid?
+  end
+
 end
