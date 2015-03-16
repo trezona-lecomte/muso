5bcf693da1091206da33dd756837fb7a0051311a
trezona-lecomte <trezona.lecomte@gmail.com>
2015-03-16 21:39:36 +1300

    Validate artist name is present

--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -2,8 +2,17 @@ require 'test_helper'
 
 class ArtistTest < ActiveSupport::TestCase
 
+  setup do
+    @artist = Artist.new(name: "The Bee Gees")
+  end
+
+  test 'should be valid after creation' do
+    assert @artist.valid?
+  end
+
   test 'name should be valid' do
-    
+    @artist.name = " "
+    assert_not @artist.valid?
   end
 
   test 'name should be unique' do
